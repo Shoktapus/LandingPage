6776ea033e49ac88f236d5a2316c58018982a746
Shoktapus <sinclair8075@gmail.com>
2022-08-06 17:17:01 +0000

    Some progress made

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-
+import {Navbar} from "../Pages/navbar.jsx"
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import {Jumbotron} from "../Pages/jumbotron.jsx"
 
-//create your first component
+//create your first component Component names must be capitalized.
+//Components must be called in the order that you want them to appear on the page.
+//This function is used to call each new component added to the project. There is no need to rewrite 
+//the function each time for each individual component.
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar/>
+			<Jumbotron/>
 		</div>
 	);
 };
