4bce9ea853811b9adf2d9fc0ae44f5d1fa76b06f
Shoktapus <sinclair8075@gmail.com>
2022-08-08 22:19:53 +0000

    Not sure if this is finished.

--- src/js/Pages/navbar.jsx
+++ src/js/Pages/navbar.jsx
@@ -8,6 +8,7 @@ import React from "react"
 export const Navbar = () => {
 	return (
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -40,6 +41,7 @@ export const Navbar = () => {
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+  </div>
   </div>
 </nav>
 	);
