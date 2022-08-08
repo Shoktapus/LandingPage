8c938d876a599a8d7028fe115b8e1765417a9831
Shoktapus <sinclair8075@gmail.com>
2022-08-08 22:20:13 +0000

    Not sure if this is finished.

--- src/js/Pages/cards.jsx
+++ src/js/Pages/cards.jsx
@@ -4,8 +4,8 @@ export const Cards = () => {
     return (
         <div class="row">
             <div class="col 4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://picsum.photos/200/300" class="card-img-top" alt="Some random pic"></img>
+                <div class="card">
+                    <img src="https://picsum.photos/200/200" class="card-img-top" alt="Some random pic"></img>
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -14,8 +14,8 @@ export const Cards = () => {
                 </div>
             </div>
             <div class="col 4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://picsum.photos/200/300" class="card-img-top" alt="Some random pic"></img>
+                <div class="card">
+                    <img src="https://picsum.photos/200/200" class="card-img-top" alt="Some random pic"></img>
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -24,8 +24,8 @@ export const Cards = () => {
                 </div>
             </div>
             <div class="col 4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://picsum.photos/200/300" class="card-img-top" alt="Some random pic"></img>
+                <div class="card">
+                    <img src="https://picsum.photos/200/200" class="card-img-top" alt="Some random pic"></img>
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -34,14 +34,17 @@ export const Cards = () => {
                 </div>
             </div>
             <div class="col 4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://picsum.photos/200/300" class="card-img-top" alt="Some random pic"></img>
+                <div class="card">
+                    <img src="https://picsum.photos/200/200" class="card-img-top" alt="Some random pic"></img>
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
+            </div>
+            <div class="card-footer text-muted">
+                FOOTER
             </div>
         </div>
     )
